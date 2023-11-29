0269a14ff7969184316a074e6de4158fd9f4fc02
Kuleen Sasse <cometku@gmail.com>
2023-11-29 18:06:21 -0500

    Cleanup of objects

--- src/object/individual.py
+++ src/object/individual.py
@@ -1,8 +1,15 @@
 from typing import List, Tuple
 
+
 class Individual:
-    def __init__(self, team: str, id: int, header: List[str], scores: List[Tuple[List[str], List[str]]]):
+    def __init__(
+        self,
+        team: str,
+        id: int,
+        header: List[str],
+        scores: List[Tuple[List[str], List[str]]],
+    ):
         self.team = team
-        self.id = id 
+        self.id = id
         self.header = header
         self.scores = scores
--- src/object/tournament.py
+++ src/object/tournament.py
@@ -2,8 +2,16 @@ from typing import List, Tuple
 
 from src.object.individual import Individual
 
+
 class Tournament:
-    def __init__(self, tourn_id: int, packets: str, headers: List[List[str]], round_scores: List[Tuple[List[str], List[str]]], individuals: List[Individual]):
+    def __init__(
+        self,
+        tourn_id: int,
+        packets: str,
+        headers: List[List[str]],
+        round_scores: List[Tuple[List[str], List[str]]],
+        individuals: List[Individual],
+    ):
         self.id = tourn_id
         self.packets = packets
         self.headers = headers
